f14df4ae507f3161f00ac28648bd53f2bb0bd7c3
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2015-10-12 23:48:40 -0400

    Save scraper settings to database
    This is the first step in allowing multiple processes to run on
    different servers. Coming in the next commit!

--- collect_district_court_case_details.py
+++ collect_district_court_case_details.py
@@ -1,8 +1,10 @@
+import boto.utils
 import datetime
 import pymongo
 import os
 import sys
 import time
+import uuid
 from courtreader import readers
 
 # Connect to database
@@ -13,10 +15,28 @@ db = client.va_district_court_cases
 reader = readers.DistrictCourtReader()
 reader.connect()
 
+# get some info about this process
+process_id = str(uuid.uuid4())
+cwd = os.getcwd()
+ec2_id = None
+try:
+    ec2_id = boto.utils.get_instance_metadata(timeout=1, num_retries=1)['instance-id']
+except:
+    pass
+
+# create db record for this process
+db.scrapers.insert_one({
+    'process_id': process_id,
+    'cwd': cwd,
+    'ec2_id': ec2_id
+})
+
+fips_code = sys.argv[1]
+
 # Fill in cases
 while True:
     case = db.cases.find_one({
-        'FIPSCode': sys.argv[1],
+        'FIPSCode': fips_code,
         'date_collected': {'$exists': False}
     })
     if case is None: break
@@ -26,5 +46,16 @@ while True:
     case_details['date_collected'] = datetime.datetime.utcnow()
     updated_case = dict(case.items() + case_details.items())
     db.cases.replace_one({'_id': case['_id']}, updated_case)
+    db.scrapers.update_one({'process_id': process_id}, {
+        '$set': {
+            'fips_code':  fips_code,
+            'last_update':  datetime.datetime.utcnow()
+        }
+    })
     time.sleep(2)
+db.scrapers.remove({'process_id': process_id})
+db['completed_courts'].replace_one({'fips_code': fips_code}, {
+    'fips_code': fips_code,
+    'completed_time': datetime.datetime.utcnow()
+}, upsert=True)
 print 'Finished'
