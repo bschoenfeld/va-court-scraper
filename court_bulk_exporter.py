bdd8e96e5d1e7b155c487777684901fcbfabfae7
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2016-03-12 09:59:36 -0500

    Improve bulk exporter

--- court_bulk_exporter.py
+++ court_bulk_exporter.py
@@ -4,6 +4,7 @@ from courtutils.logger import get_logger
 from datetime import datetime, timedelta
 import boto3
 import csv
+import json
 import pymongo
 import os
 import sys
@@ -75,7 +76,9 @@ courts_by_fips = {court['fips_code']:court for court in courts}
 s3 = boto3.resource('s3')
 db = get_db_connection()
 
-def write_cases_to_file(cases, filename, details):
+def write_cases_to_file(cases, filename, metadata):
+    metadata['total'] = 0
+    metadata['byCourt'] = {}
     with open('./' + filename + '.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=sorted(fieldnames))
         writer.writeheader()
@@ -84,9 +87,10 @@ def write_cases_to_file(cases, filename, details):
                 print 'Error getting case details', case['case_number']
                 continue
             case['Court'] = courts_by_fips[case['court_fips']]['name']
-            if case['Court'] not in details:
-                details[case['Court']] = 0
-            details[case['Court']] += 1
+            if case['Court'] not in metadata['byCourt']:
+                metadata['byCourt'][case['Court']] = 0
+            metadata['byCourt'][case['Court']] += 1
+            metadata['total'] += 1
             for detail in case['details']:
                 new_key = detail.replace(' ', '')
                 case[new_key] = case['details'][detail]
@@ -95,27 +99,29 @@ def write_cases_to_file(cases, filename, details):
                     del case[field]
             writer.writerow(case)
 
-def upload_cases(filename, details):
-    with open('./' + filename + '_details.csv', 'w') as csvfile:
+def upload_cases(filename, metadata):
+    with open('./' + filename + '_metadata.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['Court', 'Cases'])
         writer.writeheader()
-        for court in sorted(details.keys()):
+        for court in sorted(metadata['byCourt'].keys()):
             writer.writerow({
                 'Court': court,
-                'Cases': details[court]
+                'Cases': metadata['byCourt'][court]
             })
 
     zipped_file = zipfile.ZipFile(filename + '.zip', 'w')
     zipped_file.write(filename + '.csv', filename + '.csv', zipfile.ZIP_DEFLATED)
-    zipped_file.write(filename + '_details.csv', filename + '_details.csv', zipfile.ZIP_DEFLATED)
+    zipped_file.write(filename + '_metadata.csv', filename + '_metadata.csv', zipfile.ZIP_DEFLATED)
     zipped_file.close()
+
+    metadata['filesize'] = os.path.getsize(filename + '.zip')
 
     s3.Object('virginia-court-data', filename + '.zip')\
       .put(Body=open(filename + '.zip', 'rb'), ACL='public-read', ContentType='application/zip')
 
     os.remove(filename + '.zip')
     os.remove(filename + '.csv')
-    #os.remove(filename + '_details.csv')
+    os.remove(filename + '_metadata.csv')
 
 def export_and_upload_data_by_court(court_fips):
     print courts_by_fips[court_fips]['name']
@@ -138,12 +144,28 @@ def export_and_upload_data_by_year(year):
     })
 
     filename = 'criminal_circuit_court_cases_' + str(year)
-    details = {}
-    write_cases_to_file(cases, filename, details)
-    upload_cases(filename, details)
+    metadata = {}
+    write_cases_to_file(cases, filename, metadata)
+    upload_cases(filename, metadata)
+    return metadata
 
-if len(sys.argv) > 1:
-    export_and_upload_data_by_year(int(sys.argv[1]))
+if len(sys.argv) > 2:
+    start_year = int(sys.argv[1])
+    end_year = int(sys.argv[2])
+    complete_metadata = []
+    for year in range(start_year, end_year+1):
+        print 'Export', year
+        metadata = export_and_upload_data_by_year(year)
+        complete_metadata.append({
+            'year': year,
+            'downloadLink': 'https://s3.amazonaws.com/virginia-court-data/criminal_circuit_court_cases_' + str(year) + '.zip',
+            'cases': metadata['total'],
+            'filesize': metadata['filesize'],
+            'byCourt': metadata['byCourt']
+        })
+    with open('./criminalCircuitCourtCaseExport.js', 'w') as jsonfile:
+        jsonfile.write('var virginiaCourtCaseDataSets = ')
+        jsonfile.write(json.dumps(complete_metadata))
 else:
     for court in courts:
         export_and_upload_data_by_court(court['fips_code'])
