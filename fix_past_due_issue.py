0af2f2c9366d45103b95e0ddfcf2b539b140453d
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2017-01-22 20:53:23 -0500

    Add timing info to fix past due issue script

--- fix_past_due_issue.py
+++ fix_past_due_issue.py
@@ -12,6 +12,7 @@ db = PostgresDatabase('district')
 def update_case(fips):
     cases_to_fix = db.get_cases_with_no_past_due(fips, 'criminal')
     for case_to_fix in cases_to_fix:
+        time_cap_1 = datetime.now()
         case = {
             'fips': fips,
             'case_number': case_to_fix[0],
@@ -21,12 +22,17 @@ def update_case(fips):
         case['details'] = reader.get_case_details_by_number(
             fips, 'criminal', case_to_fix[0],
             case['details_url'] if 'details_url' in case else None)
+        time_cap_2 = datetime.now()
         if 'error' in case['details']:
             log.warn('Could not collect case details for %s in %s',
                      case_to_fix[0], case['fips'])
         else:
             log.info('%s %s', fips, case['details']['CaseNumber'])
+            last_case_collected = datetime.now()
             db.replace_case_details(case, 'criminal')
+        log.info('%s %s',
+                 int((time_cap_2 - time_cap_1).total_seconds()),
+                 int((datetime.now() - time_cap_2).total_seconds()))
 
 if len(sys.argv) > 1:
     update_case(sys.argv[1])
