0ed72241dc9f540615954f58995d96401d954a41
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2017-01-25 09:45:32 -0500

    Use mechanize instead of urllib2

--- courtreader/opener.py
+++ courtreader/opener.py
@@ -1,34 +1,18 @@
 import cookielib
 import os
 import pickle
-import urllib2
+import mechanize
 
 class Opener:
-    user_agent = u"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; " + \
-        u"en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11"
-
     def __init__(self, name):
-        self.name = name
-        # Create page opener that stores cookie
-        self.cookieJar = cookielib.CookieJar()
-        cookie_processor = urllib2.HTTPCookieProcessor(self.cookieJar)
-        self.opener = urllib2.build_opener(cookie_processor)
-        self.opener.addheaders = [('User-Agent', Opener.user_agent)]
-
-        # Try to load cookies
-        #if os.path.isfile(self.name + '.cookie'):
-        #    with open(self.name + '.cookie', 'r') as f:
-        #        for cookie in pickle.loads(f.read()):
-        #            self.cookieJar.set_cookie(cookie)
+        self.opener = mechanize.Browser()
+        self.opener.set_handle_robots(False)
 
     def set_cookie(self, name, value):
-        for cookie in self.cookieJar:
-            if cookie.name == name:
-                cookie.value = value
+        self.opener.set_cookie(str(name) + '=' + str(value))
 
     def save_cookies(self):
-        with open(self.name + '.cookie', 'w') as f:
-            f.write(pickle.dumps(list(self.cookieJar)))
+        return
 
     def open(self, *args):
         url = args[0]
