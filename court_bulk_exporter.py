8adc704e9d30bede6cba57c12745a09cb11983f4
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2017-03-04 21:04:56 -0500

    Zip exported data files

--- court_bulk_exporter.py
+++ court_bulk_exporter.py
@@ -3,6 +3,7 @@ import csv
 import datetime
 import os
 import subprocess
+import zipfile
 
 from courtutils.databases.postgres import PostgresDatabase
 
@@ -17,51 +18,24 @@ def export_table(table, court_type, case_type):
     db = PostgresDatabase(court_type)
     year = datetime.datetime.now().year
     while True:
-        year -= 2
+        year -= 1
         courts = db.count_courts()
         expected_count = (366 if calendar.isleap(year) else 365) * courts
         actual_count = db.count_dates_searched_for_year(case_type, year)
         if expected_count == actual_count:
+            # Create filepaths
             filepath = '{}_{}_{}'.format(court_type, case_type, year)
             temp_filepath = filepath + '_temp.csv'
+
+            # Download data from postgres into a temp file
             download_data(table, year, temp_filepath)
-            with open(temp_filepath, 'r') as temp_file:
-                data_file = None
-                anon_data_file = None
-                data_reader = csv.DictReader(temp_file)
-                case_count = 0
-                for case in data_reader:
-                    if case_count % 500000 == 0:
-                        fieldnames = data_reader.fieldnames
 
-                        fieldnames = [field for field in fieldnames if field not in REMOVE_FIELDS]
-                        if data_file: data_file.close()
-                        data_file = open(filepath + '_{}.csv'.format(
-                            str(case_count/500000).zfill(2)), 'w')
-                        data_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
-                        data_writer.writeheader()
+            # Create partitioned data files, on of which is anonymized
+            data_filepaths, anon_data_filepaths = create_data_files(filepath, temp_filepath)
 
-                        fieldnames = [field for field in fieldnames if field not in ANON_FIELDS]
-                        if anon_data_file: anon_data_file.close()
-                        anon_data_file = open(filepath + '_anon_{}.csv'.format(
-                            str(case_count/500000).zfill(2)), 'w')
-                        anon_data_writer = csv.DictWriter(anon_data_file, fieldnames=fieldnames)
-                        anon_data_writer.writeheader()
-
-                    for field in REMOVE_FIELDS:
-                        if field in case:
-                            del case[field]
-                    data_writer.writerow(case)
-
-                    for field in ANON_FIELDS:
-                        if field in case:
-                            del case[field]
-                    anon_data_writer.writerow(case)
-
-                    case_count += 1
-            data_file.close()
-            anon_data_file.close()
-            os.remove(temp_filepath)
+            # Zip data files
+            data_zip_path = zip_data_files(filepath, data_filepaths)
+            anon_data_zip_path = zip_data_files(filepath + '_anon', anon_data_filepaths)
             break
         else:
             break
@@ -78,5 +52,60 @@ def download_data(table, year, outfile_path):
         '-c', copy_cmd
     ]
     print year, subprocess.check_output(psql_cmd)
+
+def create_data_files(filepath, temp_filepath):
+    data_filepaths = []
+    anon_data_filepaths = []
+    with open(temp_filepath, 'r') as temp_file:
+        data_file = None
+        anon_data_file = None
+        data_reader = csv.DictReader(temp_file)
+        case_count = 0
+        for case in data_reader:
+            if case_count % 500000 == 0:
+                fieldnames = data_reader.fieldnames
+
+                fieldnames = [field for field in fieldnames if field not in REMOVE_FIELDS]
+                if data_file: data_file.close()
+                data_filepaths.append('{}_{}.csv'.format(
+                    filepath, str(case_count/500000).zfill(2)
+                ))
+                data_file = open(data_filepaths[-1], 'w')
+                data_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
+                data_writer.writeheader()
+
+                fieldnames = [field for field in fieldnames if field not in ANON_FIELDS]
+                if anon_data_file: anon_data_file.close()
+                anon_data_filepaths.append('{}_anon_{}.csv'.format(
+                    filepath, str(case_count/500000).zfill(2)
+                ))
+                anon_data_file = open(anon_data_filepaths[-1], 'w')
+                anon_data_writer = csv.DictWriter(anon_data_file, fieldnames=fieldnames)
+                anon_data_writer.writeheader()
+
+            for field in REMOVE_FIELDS:
+                if field in case:
+                    del case[field]
+            data_writer.writerow(case)
+
+            for field in ANON_FIELDS:
+                if field in case:
+                    del case[field]
+            anon_data_writer.writerow(case)
+
+            case_count += 1
+    data_file.close()
+    anon_data_file.close()
+    os.remove(temp_filepath)
+    return (data_filepaths, anon_data_filepaths)
+
+def zip_data_files(filepath, data_filepaths):
+    data_zip_path = filepath + '.zip'
+    data_zip = zipfile.ZipFile(data_zip_path, 'w')
+    for path in data_filepaths:
+        data_zip.write(path, path, zipfile.ZIP_DEFLATED)
+        os.remove(path)
+    data_zip.close()
+    return data_zip_path
 
 export_table('DistrictCriminalCase', 'district', 'criminal')
