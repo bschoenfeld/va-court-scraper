55dcbaffb48658e11aef58508d709ab274b9b69f
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2026-03-24 16:14:30 -0400

    Update opener.py

--- courtreader/opener.py
+++ courtreader/opener.py
@@ -9,6 +9,19 @@ class Opener:
     def __init__(self, name):
         self.opener = mechanize.Browser(history=NoHistory())
         self.opener.set_handle_robots(False)
+        self.opener.addheaders = [
+            ('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'),
+            ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'),
+            ('Accept-Language', 'en-US,en;q=0.9'),
+            ('Sec-Ch-Ua', '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"'),
+            ('Sec-Ch-Ua-Mobile', '?0'),
+            ('Sec-Ch-Ua-Platform', '"Windows"'),
+            ('Sec-Fetch-Dest', 'document'),
+            ('Sec-Fetch-Mode', 'navigate'),
+            ('Sec-Fetch-Site', 'none'),
+            ('Sec-Fetch-User', '?1'),
+            ('Upgrade-Insecure-Requests', '1')
+        ]
 
     def set_cookie(self, name, value):
         self.opener.set_cookie(str(name) + '=' + str(value))
